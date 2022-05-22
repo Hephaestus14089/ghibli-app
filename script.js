e087119f32c6f9b40823af246a8ff9faab84b13b
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-05-23 00:47:59 +0530

    Provide a check for status code

--- script.js
+++ script.js
@@ -8,10 +8,15 @@ request.onload = function() {
     // Begin accessing JSON data here
     let data = JSON.parse(this.response);
 
-    data.forEach(movie => {
-        // Log each movie's title
-        console.log(movie.title);
-    });
+    // checking the status code
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(movie => { // callback arrow function as argument forEach 
+            // Log each movie's title
+            console.log(movie.title);
+        });
+    }
+    else
+        console.log("error");
 }
 
 // Send request
