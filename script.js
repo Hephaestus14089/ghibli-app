22431f4489dddda3314b1fbbbba78bfd9c47390c
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-05-23 00:43:20 +0530

    Checking whether data retrieval from API is working
    logging part of data to console to for the purpose of checking

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 request.onload = function() {
     // Begin accessing JSON data here
+    let data = JSON.parse(this.response);
+
+    data.forEach(movie => {
+        // Log each movie's title
+        console.log(movie.title);
+    });
 }
 
 // Send request
