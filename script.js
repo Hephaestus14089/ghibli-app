db0605f03246ba3366372d018f175207511828c3
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-05-23 16:58:30 +0530

    Configuring the front-end
    Displaying the data retrieved through the API to the front-end

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'https://github.com/taniarascia/sandbox/raw/master/ghibli/logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
 // Create a request variable and assign a new XMLHttpRequest object to it
 let request = new XMLHttpRequest();
 
@@ -11,12 +22,28 @@ request.onload = function() {
     // checking the status code
     if (request.status >= 200 && request.status < 400) {
         data.forEach(movie => { // callback arrow function as argument forEach 
-            // Log each movie's title
-            console.log(movie.title);
+            
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+
+            const p = document.createElement('p');
+            movie.description = movie.description.substring(0, 300);
+            p.textContent = `${movie.description}...`;
+
+            container.appendChild(card);
+
+            card.appendChild(h1);
+            card.appendChild(p);
         });
     }
-    else
-        console.log("error");
+    else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = "Gah, it's not working!";
+        app.appendChild(errorMessage);
+    }
 }
 
 // Send request
